40d259fd121f144795167e54f4953c02c46663d6
quentinvajou <q.vajou@gmail.com>
2026-02-11 15:31:36 +0100

    modify custom views to use django generic views

--- polls/views.py
+++ polls/views.py
@@ -2,35 +2,39 @@ from django.db.models import F
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-publication_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
 
-def detail(request, question_id: int):
+# Create your views here.
+class IndexView(generic.ListView):
+    """
+    Docstring for IndexView
+    """
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """ Return the lsat 5 published questions """
+        return Question.objects.order_by("-publication_date")[:5]
+
+
+class DetailView(generic.DetailView):
     """
     Displays a question text, with no results but with a form to vote
-    
-    :param request: Description
-    :param question_id: id of the question
     """
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(request, question_id: int):
+
+class ResultsView(generic.DetailView):
     """
-    Displays results for a particular question.
-    
-    :param request: Description
-    :param question_id: id of the question
-    :type question_id: int
+    Displays results for a particular question
     """
-    question= get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id: int):
     """
