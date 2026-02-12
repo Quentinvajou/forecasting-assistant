5ad097ef40fac407a7bfde28d5e0a30effc1811a
quentinvajou <q.vajou@gmail.com>
2026-02-12 16:35:42 +0100

    add debug toolbar third party package & fix admin page

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -14,6 +15,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+
+    @admin.display(
+            boolean=True,
+            ordering="publication_date",
+            description= "published recently ?"
+    )    
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.publication_date >= now - datetime.timedelta(days=1)
